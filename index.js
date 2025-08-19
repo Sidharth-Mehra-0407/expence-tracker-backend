7caccfac2dd5c75f3eb8eeb5a23f05135a92176b
sidharth mehra <mehrasid0407@gmail.com>
2025-08-19 14:44:50 +0530

    add start script for deployment

--- index.js
+++ index.js
@@ -37,5 +37,5 @@ app.delete('/expenses/:id', (req, res) => {
 });
 
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server is running on https://expence-tracker-backend-849x.onrender.com/:${PORT}`);
 });
